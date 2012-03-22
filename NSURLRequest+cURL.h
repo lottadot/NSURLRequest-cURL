4c7863a1000d98eaf81512452f3273fef30ac024
Andrew Carter <andrew.carter@willowtreeapps.com>
2012-03-22 16:58:05 -0400

    first commit

---
+++ NSURLRequest+cURL.h
@@ -0,0 +1,13 @@
+//
+//  NSURLRequest+cURL.h
+//  temp
+//
+//  Created by Andrew Carter on 3/22/12.
+
+#import <Foundation/Foundation.h>
+
+@interface NSURLRequest (cURL)
+
+- (NSString *)cURLCommand;
+
+@end
---
+++ NSURLRequest+cURL.m
@@ -0,0 +1,38 @@
+//
+//  NSURLRequest+cURL.m
+//  temp
+//
+//  Created by Andrew Carter on 3/22/12.
+
+#import "NSURLRequest+cURL.h"
+
+@implementation NSURLRequest (cURL)
+
+- (NSString *)cURLCommand {
+    
+    NSMutableString *string = [NSMutableString string];
+    
+    [string appendString:@"curl -k -i "];
+    
+    if ([self.HTTPMethod isEqualToString:@"POST"]) {
+        
+        NSString *params = [[NSString alloc] initWithData:self.HTTPBody encoding:NSUTF8StringEncoding];
+        [string appendFormat:@"-d \"%@\" ",params];
+        [params release];
+        
+    }
+    
+    for (NSString *headerKey in [self.allHTTPHeaderFields allKeys]) {
+        
+        NSString *value = [self.allHTTPHeaderFields objectForKey:headerKey];
+        [string appendFormat:@"-H \"%@\":\"%@\" ", headerKey, value];
+        
+    }
+    
+    [string appendString:[NSString stringWithFormat:@"\"%@\"",self.URL.absoluteString]];
+    
+    return [NSString stringWithString:string];
+    
+}
+
+@end
